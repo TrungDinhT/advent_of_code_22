3b0fe11bf918fad79eb7c223bf784a2459b2612d
TrungDinhT <dinhthanhtrung1996@gmail.com>
2023-01-20 09:23:46 +0100

    Day4: Refactor

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -11,14 +11,7 @@ pub mod part2 {
     }
 
     fn nb_pairs_overlapped_impl(file_path: &str) -> usize {
-        fs::read_to_string(file_path)
-            .unwrap()
-            .lines()
-            .filter(|pair| {
-                let (first, second): (Range, Range) = split_in_two(pair, ',').unwrap();
-                first.overlap(&second)
-            })
-            .count()
+        nb_pairs_problematic_impl(file_path, Range::overlapped)
     }
 
     #[test]
@@ -35,20 +28,27 @@ pub mod part1 {
     }
 
     fn nb_pairs_fully_overlapped_impl(file_path: &str) -> usize {
-        fs::read_to_string(file_path)
-            .unwrap()
-            .lines()
-            .filter(|pair| {
-                let (first, second): (Range, Range) = split_in_two(pair, ',').unwrap();
-                first.fully_contains(&second) || second.fully_contains(&first)
-            })
-            .count()
+        nb_pairs_problematic_impl(file_path, Range::fully_overlapped)
     }
 
     #[test]
     fn test_nb_pairs_fully_overlapped_impl() {
         assert_eq!(2, nb_pairs_fully_overlapped_impl("src/day4/input_test.txt"));
     }
+}
+
+fn nb_pairs_problematic_impl<P>(file_path: &str, predicate: P) -> usize
+where
+    P: Fn(&Range, &Range) -> bool,
+{
+    fs::read_to_string(file_path)
+        .unwrap()
+        .lines()
+        .filter(|pair| {
+            let (first, second): (Range, Range) = split_in_two(pair, ',').unwrap();
+            predicate(&first, &second)
+        })
+        .count()
 }
 
 struct Range {
@@ -66,11 +66,12 @@ impl FromStr for Range {
 }
 
 impl Range {
-    fn fully_contains(&self, other: &Self) -> bool {
-        self.start <= other.start && self.end >= other.end
+    fn fully_overlapped(&self, other: &Self) -> bool {
+        (self.start <= other.start && self.end >= other.end)
+            || (self.start >= other.start && self.end <= other.end)
     }
 
-    fn overlap(&self, other: &Self) -> bool {
+    fn overlapped(&self, other: &Self) -> bool {
         self.end >= other.start && self.start <= other.end
     }
 }
@@ -79,11 +80,11 @@ fn split_in_two<T, E>(s: &str, sep: char) -> Result<(T, T), E>
 where
     T: FromStr<Err = E>,
 {
-    if let Some((first_or_error, second_or_error)) = s.split(sep).map(T::from_str).collect_tuple() {
-        let first = first_or_error?;
-        let second = second_or_error?;
+    if let Some((first_str, second_str)) = s.split(sep).collect_tuple() {
+        let first = T::from_str(first_str)?;
+        let second = T::from_str(second_str)?;
         Ok((first, second))
     } else {
-        panic!("Cannot split '{}' in two parts separated by {}", s, sep)
+        panic!("Cannot split '{}' in two parts separated by '{}'", s, sep)
     }
 }

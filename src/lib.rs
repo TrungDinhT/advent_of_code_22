6fd7bfd0fb030b6f1cc80c63b0c5cc73369e8caf
TrungDinhT <dinhthanhtrung1996@gmail.com>
2023-02-08 01:15:47 +0100

    Beautify lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -15,28 +15,32 @@ pub fn run(day: u8) {
         }
         2 => {
             println!(
-        "Day2:\n Total score if following blindly strategy guide: {}\n Total score if following correctly strategy guide: {}",
-        day2::part1::total_score(),
-        day2::part2::total_score(),
-        );
+                "Day2:\n Total score if following blindly strategy guide: {}\n \
+        Total score if following correctly strategy guide: {}",
+                day2::part1::total_score(),
+                day2::part2::total_score(),
+            );
         }
         3 => {
             println!(
-                "Day3:\n Sum of the priorities of wrongly shared items between 2 compartments: {}\n Sum of badges priorities: {}",
+                "Day3:\n Sum of the priorities of wrongly shared items between 2 compartments: {}\n \
+                Sum of badges priorities: {}",
                 day3::part1::sum_shared_items_priorities(),
                 day3::part2::sum_badges_priorities(),
             );
         }
         4 => {
             println!(
-                "Day4:\n Number of assignment pairs where one range fully contains the other: {}\n Number of overlapped assignment pairs: {}",
+                "Day4:\n Number of assignment pairs where one range fully contains the other: {}\n \
+                Number of overlapped assignment pairs: {}",
                 day4::part1::nb_pairs_fully_overlapped(),
                 day4::part2::nb_pairs_overlapped()
             );
         }
         5 => {
             println!(
-                "Day5:\n Top of warehouse stacks after rearrangement with M9000: {} \n Top of warehouse stacks after rearrangement with M9001: {} ",
+                "Day5:\n Top of warehouse stacks after rearrangement with M9000: {} \n \
+                Top of warehouse stacks after rearrangement with M9001: {} ",
                 day5::part1::top_crates_after_rearrangement(),
                 day5::part2::top_crates_after_rearrangement(),
             );

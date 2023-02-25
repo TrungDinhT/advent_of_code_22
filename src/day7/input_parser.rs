6055f351bdc00e63d5674bc1ef756e5cc4a18fe1
TrungDinhT <dinhthanhtrung1996@gmail.com>
2023-02-25 17:32:33 +0100

    Day7: Simplify input_parser

--- src/day7/input_parser.rs
+++ src/day7/input_parser.rs
@@ -1,9 +1,5 @@
-use crate::day7::input_parser::Cmd::{CD, LS};
 use itertools::Itertools;
 use std::fs;
-
-struct NodePath;
-struct NodeContent;
 
 enum Cmd {
     CD(String),
@@ -11,8 +7,8 @@ enum Cmd {
 }
 
 fn read_and_split_by_command(path: &str) -> Vec<Cmd> {
-    let input_str = fs::read_to_string(path).unwrap();
-    let content = input_str
+    fs::read_to_string(path)
+        .unwrap()
         .trim()
         .split("$")
         .skip(1) // the text start with $, so we skip the first elm split
@@ -20,30 +16,30 @@ fn read_and_split_by_command(path: &str) -> Vec<Cmd> {
             let s = s.trim();
             if s.starts_with("cd") {
                 let (_cd, path) = s.split_whitespace().collect_tuple().unwrap();
-                CD(String::from(path))
+                Cmd::CD(String::from(path))
             } else if s.starts_with("ls") {
                 // skip first line because it's `ls`
-                LS(s.lines().skip(1).map(String::from).collect())
+                Cmd::LS(s.lines().skip(1).map(String::from).collect())
             } else {
                 panic!("Unknown command: {}", s);
             }
-        });
-    for elm in content {
-        match elm {
-            CD(node_path) => println!("cd {}", node_path),
-            LS(ls_results) => println!("ls\n\t{}", ls_results.join("\n\t")),
-        };
-    }
-    vec![]
+        })
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day7::input_parser::read_and_split_by_command;
+    use super::*;
 
     #[test]
     fn test_read_and_split_by_command() {
         let output = read_and_split_by_command("src/day7/input_test.txt");
+        for elm in output.iter() {
+            match &elm {
+                Cmd::CD(node_path) => println!("cd {}", node_path),
+                Cmd::LS(ls_results) => println!("ls\n\t{}", ls_results.join("\n\t")),
+            };
+        }
         assert!(!output.is_empty());
     }
 }
